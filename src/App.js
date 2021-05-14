e8389b060ca7b93c4fef9b9bdeae8456b9baf963
asmansou <asmae.mansouri92@gmail.com>
2021-05-14 15:51:13 +0000

    expressions & conditionals in jsx

--- src/App.js
+++ src/App.js
@@ -3,14 +3,19 @@ import './App.css';
 
 class App extends Component {
   render() {
+    const name = 'John Doe';
+    const loading = true;
+    const showName = true;
+
     return (
-      // <div className='App'>
-      //   <h1>Hello from react</h1>
-      // </div>
+      // 'showName && name' is equivalent to 'showName ? name : null'
+      <div className='App'>
+        {loading ? <h1>Hello {showName && name}</h1> : <h4>Loading....</h4>}
+      </div>
       // Fragment is used to not show the parent div in the HTML
-      <Fragment>
-        <h1>Hello from react v2</h1>
-      </Fragment>
+      // <Fragment>
+      //   <h1>Hello from react v2</h1>
+      // </Fragment>
     );
   }
 }
