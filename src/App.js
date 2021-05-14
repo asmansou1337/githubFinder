16eeb5152f0c14695c569cfb7efd2809254f4ee1
asmansou <asmae.mansouri92@gmail.com>
2021-05-14 15:38:40 +0000

    intro to jsx

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import React, { Component } from 'react';
+import React, { Fragment, Component } from 'react';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className='App'>
-        <h1>Hello from react</h1>
-      </div>
+      // <div className='App'>
+      //   <h1>Hello from react</h1>
+      // </div>
+      // Fragment is used to not show the parent div in the HTML
+      <Fragment>
+        <h1>Hello from react v2</h1>
+      </Fragment>
     );
   }
 }
