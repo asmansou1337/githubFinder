65701ac673f03c6f32e2bf7a5a61690760e71a83
asmansou <asmae.mansouri92@gmail.com>
2021-05-17 11:06:55 +0100

    components & props & prototypes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
 import React, { Fragment, Component } from 'react';
+import Navbar from './components/layout/Navbar';
 import './App.css';
 
 class App extends Component {
   render() {
-    const name = 'John Doe';
-    const loading = true;
-    const showName = true;
-
-    return (
-      // 'showName && name' is equivalent to 'showName ? name : null'
-      <div className='App'>
-        {loading ? <h1>Hello {showName && name}</h1> : <h4>Loading....</h4>}
-      </div>
-      // Fragment is used to not show the parent div in the HTML
-      // <Fragment>
-      //   <h1>Hello from react v2</h1>
-      // </Fragment>
-    );
+    return <Navbar title='Github Finder' icon='fab fa-github' />;
   }
 }
 
